c320971e7fdb5ba68d1a0c3f9dbf018d1968af7a
Aliseiko <aliseiko.dev@gmail.com>
2022-04-16 18:11:06 +0300

    feat: add ru and en keys list, keyboard keymap

---
+++ js/ru.js
@@ -0,0 +1,322 @@
+const ruKeys = [
+  {
+    code: 'Backquote',
+    default: 'ё',
+    shift: 'Ё',
+  },
+  {
+    code: 'Digit1',
+    default: '1',
+    shift: '!',
+  },
+  {
+    code: 'Digit2',
+    default: '2',
+    shift: '@',
+  },
+  {
+    code: 'Digit3',
+    default: '3',
+    shift: '№',
+  },
+  {
+    code: 'Digit4',
+    default: '4',
+    shift: ';',
+  },
+  {
+    code: 'Digit5',
+    default: '5',
+    shift: '%',
+  },
+  {
+    code: 'Digit6',
+    default: '6',
+    shift: ':',
+  },
+  {
+    code: 'Digit7',
+    default: '7',
+    shift: '?',
+  },
+  {
+    code: 'Digit8',
+    default: '8',
+    shift: '*',
+  },
+  {
+    code: 'Digit9',
+    default: '9',
+    shift: '(',
+  },
+  {
+    code: 'Digit0',
+    default: '0',
+    shift: ')',
+  },
+  {
+    code: 'Minus',
+    default: '-',
+    shift: '_',
+  },
+  {
+    code: 'Equal',
+    default: '=',
+    shift: '+',
+  },
+  {
+    code: 'Backspace',
+    default: 'Backspace',
+    shift: 'Backspace',
+  },
+  {
+    code: 'Tab',
+    default: 'Tab',
+    shift: 'Tab',
+  },
+  {
+    code: 'KeyQ',
+    default: 'й',
+    shift: 'Й',
+  },
+  {
+    code: 'KeyW',
+    default: 'ц',
+    shift: 'Ц',
+  },
+  {
+    code: 'KeyE',
+    default: 'у',
+    shift: 'У',
+  },
+  {
+    code: 'KeyR',
+    default: 'к',
+    shift: 'К',
+  },
+  {
+    code: 'KeyT',
+    default: 'е',
+    shift: 'Е',
+  },
+  {
+    code: 'KeyY',
+    default: 'н',
+    shift: 'Н',
+  },
+  {
+    code: 'KeyU',
+    default: 'г',
+    shift: 'Г',
+  },
+  {
+    code: 'KeyI',
+    default: 'ш',
+    shift: 'Ш',
+  },
+  {
+    code: 'KeyO',
+    default: 'щ',
+    shift: 'Щ',
+  },
+  {
+    code: 'KeyP',
+    default: 'з',
+    shift: 'З',
+  },
+  {
+    code: 'BracketLeft',
+    default: 'х',
+    shift: 'Х',
+  },
+  {
+    code: 'BracketRight',
+    default: 'ъ',
+    shift: 'Ъ',
+  },
+  {
+    code: 'Backslash',
+    default: '\\',
+    shift: '/',
+  },
+  {
+    code: 'Delete',
+    default: 'Del',
+    shift: 'Del',
+  },
+  {
+    code: 'CapsLock',
+    default: 'Capslk',
+    shift: 'Cpslk',
+  },
+  {
+    code: 'KeyA',
+    default: 'ф',
+    shift: 'Ф',
+  },
+  {
+    code: 'KeyS',
+    default: 'ы',
+    shift: 'Ы',
+  },
+  {
+    code: 'KeyD',
+    default: 'в',
+    shift: 'В',
+  },
+  {
+    code: 'KeyF',
+    default: 'а',
+    shift: 'А',
+  },
+  {
+    code: 'KeyG',
+    default: 'п',
+    shift: 'П',
+  },
+  {
+    code: 'KeyH',
+    default: 'р',
+    shift: 'Р',
+  },
+  {
+    code: 'KeyJ',
+    default: 'о',
+    shift: 'О',
+  },
+  {
+    code: 'KeyK',
+    default: 'л',
+    shift: 'Л',
+  },
+  {
+    code: 'KeyL',
+    default: 'д',
+    shift: 'Д',
+  },
+  {
+    code: 'Semicolon',
+    default: 'ж',
+    shift: 'Ж',
+  },
+  {
+    code: 'Quote',
+    default: 'э',
+    shift: 'Э',
+  },
+  {
+    code: 'Enter',
+    default: 'Enter',
+    shift: 'Enter',
+  },
+  {
+    code: 'ShiftLeft',
+    default: 'Shift',
+    shift: 'Shift',
+  },
+  {
+    code: 'KeyZ',
+    default: 'я',
+    shift: 'Я',
+  },
+  {
+    code: 'KeyX',
+    default: 'ч',
+    shift: 'Ч',
+  },
+  {
+    code: 'KeyC',
+    default: 'с',
+    shift: 'С',
+  },
+  {
+    code: 'KeyV',
+    default: 'м',
+    shift: 'М',
+  },
+  {
+    code: 'KeyB',
+    default: 'и',
+    shift: 'И',
+  },
+  {
+    code: 'KeyN',
+    default: 'т',
+    shift: 'Т',
+  },
+  {
+    code: 'KeyM',
+    default: 'ь',
+    shift: 'Ь',
+  },
+  {
+    code: 'Comma',
+    default: 'б',
+    shift: 'Б',
+  },
+  {
+    code: 'Period',
+    default: 'ю',
+    shift: 'Ю',
+  },
+  {
+    code: 'Slash',
+    default: '.',
+    shift: ',',
+  },
+  {
+    code: 'ArrowUp',
+    default: '↑',
+    shift: '↑',
+  },
+  {
+    code: 'ShiftRight',
+    default: 'Shift',
+    shift: 'Shift',
+  },
+  {
+    code: 'ControlLeft',
+    default: 'Ctrl',
+    shift: 'Ctrl',
+  },
+  {
+    code: 'MetaLeft',
+    default: 'Win',
+    shift: 'Win',
+  },
+  {
+    code: 'AltLeft',
+    default: 'Alt',
+    shift: 'Alt',
+  },
+  {
+    code: 'Space',
+    default: 'Space',
+    shift: 'Space',
+  },
+  {
+    code: 'AltRight',
+    default: 'Alt',
+    shift: 'Alt',
+  },
+  {
+    code: 'ControlRight',
+    default: 'Ctrl',
+    shift: 'Ctrl',
+  },
+  {
+    code: 'ArrowLeft',
+    default: '←',
+    shift: '←',
+  },
+  {
+    code: 'ArrowDown',
+    default: '↓',
+    shift: '↓',
+  },
+  {
+    code: 'ArrowRight',
+    default: '→',
+    shift: '→',
+  },
+];
