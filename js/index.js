88b115135748d0673310c6357eaf652dd46f5a28
Aliseiko <aliseiko.dev@gmail.com>
2022-04-17 18:53:47 +0300

    refactor: remove unused container from html, rename lang js files

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+import languages from './languages';
+
 const keyMap = [
   ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'],
   ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete'],
