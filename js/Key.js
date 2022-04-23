ee7134be192753ca2d54d14b6580243b75e59ed0
Aliseiko <aliseiko.dev@gmail.com>
2022-04-23 15:24:18 +0300

    feat: add keyboard class, add keyboard to page

--- js/Key.js
+++ js/Key.js
@@ -1,19 +1,20 @@
-const fnKeys = ['AltLeft', 'AltRight', 'ArrowUp', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'Backspace', 'CapsLock', 'ControlLeft', 'ControlRight', 'Delete', 'Enter', 'ShiftLeft', 'ShiftRight', 'Tab', 'MetaLeft'];
+/* eslint-disable import/extensions */
+import languages from './lang/languages.js';
+import createElement from './utils/createElement.js';
+
+const fnKeys = ['AltLeft', 'AltRight', 'ArrowUp', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'Backspace', 'CapsLock', 'ControlLeft', 'ControlRight', 'Delete', 'Enter', 'ShiftLeft', 'ShiftRight', 'Tab', 'MetaLeft', 'Space'];
 
 export default class Key {
-  constructor(small, shift, code) {
-    this.small = small;
-    this.shift = shift;
+  constructor(langCode, code) {
+    this.small = languages[langCode].find((el) => el.code === code).small;
+    this.shift = languages[langCode].find((el) => el.code === code).shift;
     this.code = code;
     this.isFnKey = fnKeys.includes(code);
   }
 
   createKey() {
-    const key = document.createElement('div');
-    key.classList.add('key');
+    const key = createElement('div', 'key', 'code', this.code, this.small);
     if (this.isFnKey) key.classList.add(this.code);
-    key.dataset.code = this.code;
-    key.innerHTML = this.small;
     return key;
   }
 }
