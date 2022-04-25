240851494c506a2ccfb8c193940d76ba0e789511
Aliseiko <aliseiko.dev@gmail.com>
2022-04-25 23:18:25 +0300

    refactor: print algorithm

--- js/Keyboard.js
+++ js/Keyboard.js
@@ -2,16 +2,21 @@
 import Key from './Key.js';
 import createElement from './utils/createElement.js';
 
-const keyMap = [
-  ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'],
-  ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete'],
-  ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter'],
-  ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight'],
-  ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'],
-];
-
 export default class Keyboard {
   constructor(langCode) {
+    this.keyMap = [
+      ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'],
+      ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete'],
+      ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter'],
+      ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight'],
+      ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'],
+    ];
+    this.specialCharacters = {
+      Enter: '\n',
+      Space: ' ',
+      Tab: '\t',
+    };
+    this.arrowKeys = ['ArrowUp', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
     this.currentLang = langCode;
     this.pressedKeys = [];
     this.keyboardKeysObj = [];
@@ -23,14 +28,14 @@ export default class Keyboard {
     const description = createElement('p', 'description', null, null, 'Keyboard for Windows operating system');
     const languageDescription = createElement('p', 'language', null, null, 'Switch language on click: left ctrl + shift');
     this.textarea = createElement('textarea', 'textarea');
-    document.body.append(title, this.textarea, keyboard, description, languageDescription);
+    document.body.prepend(title, this.textarea, keyboard, description, languageDescription);
     this.textarea.focus();
     keyboard.addEventListener('mousedown', this.handleEvents);
   }
 
   createKeyboard() {
     const keyboard = createElement('div', 'keyboard', 'lang', this.currentLang);
-    keyMap.forEach((rowKeys) => {
+    this.keyMap.forEach((rowKeys) => {
       const row = createElement('div', 'row');
       rowKeys.forEach((key) => {
         const newKey = new Key(this.currentLang, key);
@@ -47,33 +52,26 @@ export default class Keyboard {
     if (!e.target.classList.contains('key')) return;
     const key = e.target;
 
-    function deactivateKey() {
+    const deactivateKey = () => {
       key.classList.remove('active');
       key.removeEventListener('mouseleave', deactivateKey);
       key.removeEventListener('mouseup', deactivateKey);
-    }
+      this.textarea.focus();
+    };
+
+    const insertChar = (char) => this.textarea.setRangeText(char, this.textarea.selectionStart, this.textarea.selectionEnd, 'end');
 
     e.stopPropagation();
     key.classList.add('active');
+    this.textarea.focus();
 
     if (!key.dataset.isFnKey
-        || key.dataset.code === 'ArrowDown'
-        || key.dataset.code === 'ArrowRight'
-        || key.dataset.code === 'ArrowUp'
-        || key.dataset.code === 'ArrowLeft') {
-      this.pressedKeys.push(key.textContent);
-    } else if (key.dataset.code === 'Enter') {
-      this.pressedKeys.push('\n');
-    } else if (key.dataset.code === 'Space') {
-      this.pressedKeys.push(' ');
-    } else if (key.dataset.code === 'Tab') {
-      this.pressedKeys.push('\t');
-    } else if (key.dataset.code === 'Backspace'
-               && this.pressedKeys.length !== 0) {
-      this.pressedKeys.pop();
+        || this.arrowKeys.includes(key.dataset.code)) {
+      insertChar(key.textContent);
+    } else if (this.specialCharacters[key.dataset.code]) {
+      insertChar(this.specialCharacters[key.dataset.code]);
     }
 
-    this.textarea.value = this.pressedKeys.join('');
     key.addEventListener('mouseleave', deactivateKey);
     key.addEventListener('mouseup', deactivateKey);
   };
